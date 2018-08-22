2595621a1fe6c05578c513878e22605a25a28ed2
Tarun <tarun.mohandas@gmail.com>
2018-08-22 20:52:16 +0530

    makefile created

---
+++ src/hellomake.cpp
@@ -0,0 +1,7 @@
+#include "../headers/hellomake.h"
+int add(int,int);
+int main()
+{
+	cout<<"\n\nHello Geeks!!\n\n";
+	cout<<endl<<add(2,3)<<endl;
+}
---
+++ src/hellomake2.cpp
@@ -0,0 +1,5 @@
+#include "../headers/hellomake.h"
+int add(int a, int b)
+{
+	return a+b;
+}
