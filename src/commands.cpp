de5023c359999990083c6c0ac3ef1ed6c6ca8dfd
tarun0409 <tarun.mohandas@gmail.com>
2018-08-26 23:44:52 +0530

    copy file feature enabled

--- src/commands.cpp
+++ src/commands.cpp
@@ -116,4 +116,51 @@ void exec_command(string cmd)
 		FILE * f = fopen(convert_string_to_char(file_name),"w+");
 		fclose(f);
 	}
+	if(!op.compare("create_dir"))
+	{
+		string fold_name = cmd_split[1];
+		string dir_name = cmd_split[2];
+		if(!dir_name.compare("."))
+		{
+			string cwd = getcwd(NULL,100*sizeof(char));
+			cwd+='/';
+			fold_name = cwd.append(fold_name);
+		}
+		else
+		{
+			string root_dir = get_root_dir();
+			dir_name = root_dir.append(dir_name);
+			dir_name+='/';
+			fold_name = dir_name.append(fold_name);
+		}
+		mode_t mode = 0777 & ~umask(0);
+		mode = mode | S_IWUSR | S_IXUSR;
+		mkdir(convert_string_to_char(fold_name),mode);
+	}
+	if(!op.compare("copy"))
+	{
+		int n = cmd_split.size();
+		for(int i=1; i<(n-1); i++)
+		{
+			string dir_name = cmd_split[n-1];
+			string root_dir = get_root_dir();
+			dir_name = root_dir.append(dir_name);
+			dir_name+='/';
+			string file_name = cmd_split[i];
+			char out_buff[1024];
+			int r_fd = open(convert_string_to_char(file_name),O_RDONLY);
+			string new_file_name = file_name;
+			new_file_name = dir_name.append(new_file_name);
+			mode_t mode = 0777 & ~umask(0);
+			int w_fd = open(convert_string_to_char(new_file_name),(O_WRONLY|O_CREAT),mode);
+			int r_size = (read(r_fd,out_buff,sizeof(out_buff)));
+			while(r_size > 0)
+			{
+				write(w_fd,out_buff,r_size);
+				r_size = (read(r_fd,out_buff,sizeof(out_buff)));
+			}
+			close(r_fd);
+			close(w_fd);
+		}
+	}
 }
